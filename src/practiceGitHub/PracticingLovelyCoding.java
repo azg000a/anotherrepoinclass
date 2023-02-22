5c97bbf3c2ea5b59cf678c09528ee946f8d0208e
Ahmad <Ahmad.Ghafoor@tekschool.us>
2023-02-21 21:23:00 -0500

    my first commit for this project

---
+++ src/practiceGitHub/PracticingLovelyCoding.java
@@ -0,0 +1,10 @@
+package practiceGitHub;
+
+public class PracticingLovelyCoding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I am trying");
+	}
+
+}
